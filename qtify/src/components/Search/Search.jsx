789307b69f2acb8852521d5cf2a0c51d715eb24b
chaitanya <chaitanyasrinivas30@gmail.com>
2026-07-25 03:01:39 +0530

    Initial commit

--- qtify/src/components/Search/Search.jsx
+++ qtify/src/components/Search/Search.jsx
@@ -39,7 +39,7 @@ const Listbox = styled("ul")(({ theme }) => ({
 function Search({ searchData, placeholder }) {
   const {
     getRootProps,
-    getInputLabelProps,
+    
     value,
     getInputProps,
     getListboxProps,
