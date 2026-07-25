747a456141fa4ff444fca1fd958c8ab59fd0593b
chaitanya <chaitanyasrinivas30@gmail.com>
2026-07-25 13:27:05 +0530

    Initial commit

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,9 +1,8 @@
-import React from "react";
 import styles from "./Button.module.css";
 
-function Button({ children, onClick }) {
+function Button({ children }) {
   return (
-    <button className={styles.button} onClick={onClick}>
+    <button className={styles.button}>
       {children}
     </button>
   );
